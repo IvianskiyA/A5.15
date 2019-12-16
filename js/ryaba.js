5d84a8531f8380d3f66bfcacff873c5877017749
IvianskiyA <IvianskiyA@mail.ru>
2019-12-17 01:37:16 +0300

    Update ryaba.js

--- js/ryaba.js
+++ js/ryaba.js
@@ -1,20 +1,26 @@
 const dataURL = "https://api.myjson.com/bins/jcmhn";
 
 function handleButton() {
+	$.getJSON(dataURL, 
   // взять данные по dataUrl, вытащить их и передать в handleData
-}
+
 
 function handleData(data) {
   // кажется, какой-то из этих способов сработает
   //const var1 = $("input[name=var1]")[0].value()
-  //const var1 = $("input[name=var1]").value()
+  const var1 = $("input[name=var1]").val()
   //const var1 = $("input[name=var1]")[0].default()
 
   // надо сделать так чтобы сообщения подменились на значения из формы
-  let text = "Здесь могла быть ваша реклама";
-	$("#result").text(text);
+  console.log(var1);
+  //render(data);
+  console.log(data)  
+  const text = $(data);
+  	$("#result").text(text);
 }
+)
 
+}
 function init() {
 	$("#button-fetch").click(handleButton);
 }
